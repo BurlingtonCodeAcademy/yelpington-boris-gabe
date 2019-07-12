e52e77b85beaf1ea040cc48bc3d8b4b6987356ed
Gabe N <nelsong1997@gmail.com>
2019-07-12 15:29:40 -0400

    transfer to boris

--- main.js
+++ main.js
@@ -1,79 +1,42 @@
 fetch("all.json")
-  .then(response => response.json())
-  .then(jso => {
-    restaurantList = jso;
-    if (restaurantList.includes(window.location.hash.slice(1))) {
-      let restaurantName = window.location.hash.slice(1);
-      fetch(`${restaurantName}.json`)
-        .then(response => response.json())
-        .then(jso => {
-          createRestaurantPage(jso);
-        });
-    } else {
-      console.log("404");
-    }
+.then(response => response.json())
+.then(jso => {
+  let restaurantList = jso;
+  createList();
+  let restaurantNamePromises = restaurantList.map(findRestaurantInfo);
+  Promise.all(restaurantNamePromises)
+    .then(function (nameProperties) {
+      nameProperties.forEach(createElement)
+      nameProperties.forEach(createMarker)
   });
+});
+    
 
-
-function createRestaurantPage(restaurantInfo) {
-  let element = document.getElementById("allInfo");
-  let name = document.createElement("h1");
-  let address = document.createElement("p");
-  let phoneLabel = document.createElement("h4");
-  let phone = document.createElement("a");
-  let hoursLabel = document.createElement("h4");
-  let hours = document.createElement("a");
-  let websiteLabel = document.createElement("h4");
-  let website = document.createElement("a");
-  let noteLabel = document.createElement("h3");
-  let notes = document.createElement("ul");
+function createList () {
+  let element = document.getElementById("all-restaurants");
+  let list = document.createElement('ul');
+  element.appendChild(list);
+}
   
-  document.querySelector('title').textContent = restaurantInfo.name;
-  name.textContent = restaurantInfo.name;
-  address.textContent = restaurantInfo.address;
-  phoneLabel.textContent = "Phone number : ";
-  phone.textContent = restaurantInfo.phone;
-  phone.href = `tel:${restaurantInfo.phone}`;
-  hoursLabel.textContent = "Hours : ";
-  if (restaurantInfo.hours !== undefined) {
-    hours.textContent = restaurantInfo.hours;
-  } else {
-    hours.textContent = "We show up whenever we feel like it!";
-  }
-  websiteLabel.textContent = "Website : ";
-  if (restaurantInfo.website !== undefined) {
-    website.textContent = restaurantInfo.website;
-    website.href = restaurantInfo.website;
-  } else {
-    website.textContent = "Website coming soon!";
-  }
-  noteLabel.textContent = "Notes : ";
+function createElement (restaurantInfo) {
+  let list = document.querySelector('ul');
+  let listItem = document.createElement('li');
+  let website = document.createElement('a');
+  
+      website.textContent = restaurantInfo.name;
+      website.href = `/restaurant.html#${restaurantInfo.id}`;
+      
+      list.appendChild(listItem);
+      listItem.appendChild(website);
+    }
 
-  restaurantInfo.notes.forEach(arrayItem => {
-    let listItem = document.createElement("li");
-    notes.appendChild(listItem);
-    listItem.textContent = arrayItem;
-    console.log(element);
-  });
+function findRestaurantInfo (restaurant) {
+  return fetch(`./${restaurant}.json`)
+  .then(response => response.json())
+}
 
-  element.appendChild(name);
-  element.appendChild(address);
-  element.appendChild(phoneLabel);
-  element.appendChild(phone);
-  element.appendChild(document.createElement("br"));
-  element.appendChild(hoursLabel);
-  element.appendChild(hours);
-  element.appendChild(document.createElement("br"));
-  element.appendChild(websiteLabel);
-  element.appendChild(website);
-  element.appendChild(noteLabel);
-  element.appendChild(notes);
-
-  fetch(
-    `https://nominatim.openstreetmap.org/search/?q=${
-      restaurantInfo.address
-    }&format=json`
-  )
+function createMarker(restaurantInfo) {
+  fetch(`https://nominatim.openstreetmap.org/search/?q=${restaurantInfo.address}&format=json`)
     .then(response => response.json())
     .then(jso => {
       makeMap(jso[0]["lat"], jso[0]["lon"]);
@@ -89,7 +52,5 @@ function createRestaurantPage(restaurantInfo) {
     }).addTo(mymap);
 
     var marker = L.marker([lat, long]).addTo(mymap);
-
-    marker.bindPopup("Surprise!");
   }
 }
